c4f4611e98ad4f9b9a87637e8373174663166b48
mintsweet <0x1304570@gmail.com>
2025-01-21 11:56:58 +1300

    fix: file name error

--- backend/src/ocr/ocr.module.ts
+++ backend/src/ocr/ocr.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { OcrController } from './orc.controller';
+import { OcrController } from './ocr.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
